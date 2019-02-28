8c1e236925f0dbe16344b7985bce8b2edd355d68
William Lake <noreply>
2019-02-28 13:47:15 -0700

    Resolved Issue #2

--- src/main/java/com/lakedev/KnowledgeBase/KnowledgeBaseApplication.java
+++ src/main/java/com/lakedev/KnowledgeBase/KnowledgeBaseApplication.java
@@ -6,8 +6,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KnowledgeBaseApplication 
 {
+	private static final String PORT_ARG_STRING = "--server.port=";
+	
+	private static final String DEFAULT_PORT = "8081";
+	
 	public static void main(String[] args) 
 	{
+		/*
+		 * We need to see if the server.port argument was passed.
+		 * If not, we need to add it with the default port.
+		 */
+		boolean portArgExists = false;
+		
+		for (String arg : args)
+		{
+			if (arg.toUpperCase().contains(PORT_ARG_STRING.toUpperCase()))
+			{
+				portArgExists = true;
+				
+				break;
+			}
+		}
+		
+		if (portArgExists == false)
+		{
+			String portArg = PORT_ARG_STRING + DEFAULT_PORT;
+			
+			String[] tmpArgs = new String[args.length + 1];
+			
+			for (int argIndex = 0; argIndex < args.length; argIndex++)
+			{
+				tmpArgs[argIndex] = args[argIndex];
+			}
+			
+			tmpArgs[args.length] = portArg;
+			
+			args = tmpArgs;
+		}
+		
 		SpringApplication.run(KnowledgeBaseApplication.class, args);
 	}
 	
